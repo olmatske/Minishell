a6630c394ea608cfedc4e2025982797e59cb4a76
Anastasia Shuvalova <anshuval@1-E-18.42heilbronn.de>
2026-02-26 16:00:49 +0100

    struct

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: anshuval <anshuval@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 15:30:44 by anshuval          #+#    #+#             */
-/*   Updated: 2026/02/26 15:45:00 by anshuval         ###   ########.fr       */
+/*   Updated: 2026/02/26 16:00:17 by anshuval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@
 
 typedef struct s_cmd {
 	char	**args;
-	
-}
+	t_redir	*redir;
+} t_cmd;
 
 int		main(int argc, char **argv, char **envp);
 
