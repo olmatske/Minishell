2524696f73b0beddb45a4a809c1512a54165c9ee
Anastasia Shuvalova <anshuval@1-E-18.42heilbronn.de>
2026-02-26 16:24:58 +0100

    trying to solve the branch problem

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: anshuval <anshuval@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 15:30:44 by anshuval          #+#    #+#             */
-/*   Updated: 2026/02/26 15:45:00 by anshuval         ###   ########.fr       */
+/*   Updated: 2026/02/26 16:23:21 by anshuval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,27 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <fcntl.h>
-# include <sys/types.h>
-# include <sys/wait.h>
 # include <string.h>
 
-typedef struct s_cmd {
-	char	**args;
+typedef struct s_redir {
 	
-}
+} t_redir;
+
+typedef struct s_built_in {
+	
+} t_built_in;
+
+typedef struct s_cmd {
+	char		**args;
+	t_redir		*redir;
+	t_built_in	built_in;
+} t_cmd;
+
+typedef struct s_cmd_node {
+	t_cmd				*cmd;
+	struct s_cmd_node	*next;
+} t_cmd_node;
+
 
 int		main(int argc, char **argv, char **envp);
 
