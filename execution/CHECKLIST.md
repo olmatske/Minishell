a752aace4fcb315842ad88894ffb0903d708c26b
Olga Matskevich <olmatske@3-H-6.42heilbronn.de>
2026-03-21 20:12:59 +0100

    worked out checklist and a visualisation for structs in header file

---
+++ execution/CHECKLIST.md
@@ -0,0 +1,35 @@
+# M I N I S H E L L
+
+### Questions
+- [ ] what are built in commands?
+- [ ] hwo do pipes work exactly?
+--------------------------------------------------------------------------------
+### Functions
+- [ ] built ins
+	- [ ] cd
+	- [ ] echo
+	- [ ] env
+	- [ ] export
+	- [ ] pwd
+	- [ ] unset
+	- [ ] exit
+- [ ] expander
+- [ ] heredoc
+--------------------------------------------------------------------------------
+### Other
+- [ ] structs
+--------------------------------------------------------------------------------
+### Library
+- [ ] flags (heredoc, pipe, redicrection etc)
+- [ ] struct for built in commands
+- [ ] struct for argumnets and flags
+
+- [ ] start with: echo hi (> hi.txt)
+- [ ] continue with built ins
+- [ ] anything without env vars
+- [ ] figure out
+
+
+- [ ] execve are for external!!!!
+
+
--- execution/execution.h
+++ execution/execution.h
@@ -6,17 +6,43 @@
 /*   By: olmatske <olmatske@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 17:11:42 by olmatske          #+#    #+#             */
-/*   Updated: 2026/02/26 17:15:55 by olmatske         ###   ########.fr       */
+/*   Updated: 2026/03/21 20:09:21 by olmatske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EXECUTION_H
-#define EXECUTION_H
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
-#include <stdio.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <string.h>
+
+typedef struct s_redir {
+	// types of redirection     OUT (enum)
+	// target                   text.txt
+}	t_redir;
+
+typedef struct s_built_in {
+	
+}	t_built_in;
+
+typedef struct s_cmd {
+	char		**args; // args[0] = "cat" -> args[1] = main.c
+	t_redir		*redir; 
+	t_built_in	built_in; // nothing because cat isn't a built in function
+}	t_cmd;
+
+typedef struct s_cmd_node {
+	t_cmd				*cmd;
+	struct s_cmd_node	*next;
+}	t_cmd_node;
 
 
-
+int		main(int argc, char **argv, char **envp);
 
 #endif
+
+
+// cat main.c > text.txt
