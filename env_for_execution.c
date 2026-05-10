c242c06a290b10baefa6c778fc873db41e3fe540
Olga Matskevich <olmatske@3-F-12.42heilbronn.de>
2026-05-10 16:06:22 +0200

    commented out find_command_path and added rl_clear_history instead clear_history

--- env_for_execution.c
+++ env_for_execution.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env_for_execution.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anshuval <anshuval@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: olmatske <olmatske@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 14:23:09 by anshuval          #+#    #+#             */
-/*   Updated: 2026/05/10 15:25:17 by anshuval         ###   ########.fr       */
+/*   Updated: 2026/05/10 16:05:10 by olmatske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,32 +57,32 @@ char	**env_array_for_execution(t_env *copied_env)
 	return (env_array);
 }
 
-char	*find_command_path(t_cmd_node *cmd, t_env **env)
-{
-	char	*full_path;
-	char	**directory;
-	char	**env_array;
+// char	*find_command_path(t_cmd_node *cmd, t_env **env)
+// {
+// 	char	*full_path;
+// 	char	**directory;
+// 	char	**env_array;
 
-	env_array = env_array_for_execution(env);
-	if (ft_strchr(cmd, '/') != NULL)
-	{
-		if (access(cmd, X_OK) == 0)
-			return (ft_strdup(cmd));
-	}
-	while (*env_array && ft_strncmp(*env_array, "PATH=", 5) != 0)
-		env_array++;
-	if (*env_array == NULL)
-		return (NULL);
-	directory = ft_split(*env_array + 5, ':');
-	if (directory == NULL)
-		return (NULL);
-	full_path = search_in_directories(directory, cmd);
-	if (full_path == NULL)
-	{
-		free_env_array(env_array);
-		return (NULL);
-	}
-	ft_free_array(directory);
-	return (full_path);
-}
+// 	env_array = env_array_for_execution(env);
+// 	if (ft_strchr(cmd, '/') != NULL)
+// 	{
+// 		if (access(cmd, X_OK) == 0)
+// 			return (ft_strdup(cmd));
+// 	}
+// 	while (*env_array && ft_strncmp(*env_array, "PATH=", 5) != 0)
+// 		env_array++;
+// 	if (*env_array == NULL)
+// 		return (NULL);
+// 	directory = ft_split(*env_array + 5, ':');
+// 	if (directory == NULL)
+// 		return (NULL);
+// 	full_path = search_in_directories(directory, cmd);
+// 	if (full_path == NULL)
+// 	{
+// 		free_env_array(env_array);
+// 		return (NULL);
+// 	}
+// 	ft_free_array(directory);
+// 	return (full_path);
+// }
 
--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: olmatske <olmatske@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 15:33:42 by anshuval          #+#    #+#             */
-/*   Updated: 2026/05/10 15:53:52 by olmatske         ###   ########.fr       */
+/*   Updated: 2026/05/10 16:05:30 by olmatske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,7 @@ int	main(int argc, char **argv, char **envp)
 	if (copied_env == NULL)
 		ft_error("Failed to copy environment.\n", 1);
 	minishell_loop(copied_env, shell);
-	clear_history(); // changed from rl_clear_history to clear_history
+	rl_clear_history(); // changed from rl_clear_history to clear_history
 	free_env_list(&copied_env);
 	return (0);
 }
