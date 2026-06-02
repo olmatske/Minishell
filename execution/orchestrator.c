0fc89254a897174decacca9808294e01853e6399
Olga Matskevich <olmatske@3-G-9.42heilbronn.de>
2026-06-02 15:04:31 +0200

    redirection '>' works now

--- execution/orchestrator.c
+++ execution/orchestrator.c
@@ -6,7 +6,7 @@
 /*   By: olmatske <olmatske@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 13:57:42 by olmatske          #+#    #+#             */
-/*   Updated: 2026/05/31 14:25:41 by olmatske         ###   ########.fr       */
+/*   Updated: 2026/06/02 15:01:22 by olmatske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,19 @@ int	shell_loop(t_shell *shell, t_cmd_node *cmd_list)
 // > test hangs somewhere, idk how to fix it :,)
 int	exec_single_cmd(t_shell *shell, t_env *env, t_cmd_node *cmd_list)
 {
+	int	saved_in;
+	int	saved_out;
+
 	if (cmd_list->cmd->built_in_name == BUILTIN_NONE
-		&& !cmd_list->cmd->args && !cmd_list->cmd->args[0])
+		&& (!cmd_list->cmd->args || !cmd_list->cmd->args[0]))
 	{
+		saved_in = dup(STDIN_FILENO);
+		saved_out = dup(STDOUT_FILENO);
 		shell->exit = wrapper_redirections(cmd_list->cmd->redir);
+		dup2(saved_in, STDIN_FILENO);
+		dup2(saved_out, STDOUT_FILENO);
+		close(saved_in);
+		close(saved_out);
 		update_shell_status(shell->env, shell);
 		return (shell->exit);
 	}
@@ -39,6 +48,5 @@ int	exec_single_cmd(t_shell *shell, t_env *env, t_cmd_node *cmd_list)
 	}
 	shell->exit = exec_external(shell, cmd_list->cmd, env);
 	update_shell_status(shell->env, shell);
-	// printf("HELLOOO THIS IS A CHECKPOINT\n");
 	return (shell->exit);
 }
